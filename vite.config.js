1d93267f0aa63416086e3bbe846566d660e14c98
Abhiram <abhiram.helios@gmail.com>
2025-09-25 20:55:19 +0530

    vite sheriyakki

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or whatever plugin you use
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/cv-2025/', // <--- Add this line!
   plugins: [react()],
 })
